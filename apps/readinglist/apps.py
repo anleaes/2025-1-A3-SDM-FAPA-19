7f73426eab7448089f36e3397f8f8131b4749e5e
franciscosiq <franciscosiq@gmail.com>
2025-06-26 20:23:54 -0300

    adicionando reading list models

---
+++ apps/readinglist/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ReadinglistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'readinglist'
+    verbose_name = 'Listas de Leitura'
