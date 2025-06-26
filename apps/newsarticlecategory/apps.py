5ab55c663dae648e6ceb147fa99cbb2ff7e2354e
franciscosiq <franciscosiq@gmail.com>
2025-06-26 20:53:40 -0300

    implementando news article category models

---
+++ apps/newsarticlecategory/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NewsarticlecategoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'newsarticlecategory'
+    verbose_name = 'Categorias de Artigos de Notícias'
