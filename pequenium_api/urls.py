0dd6a4d8147dbe0c3afb09d4c06f6920d85fb8a7
franciscosiq <franciscosiq@gmail.com>
2025-06-26 18:13:25 -0300

    adicionando news article urls

--- pequenium_api/urls.py
+++ pequenium_api/urls.py
@@ -5,4 +5,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-autenticacao/', obtain_auth_token),
+    path('categorias/', include('category.urls', namespace='category')),
 ]
