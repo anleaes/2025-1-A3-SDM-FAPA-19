7641166b2393e84f54f81d2bd1f12e7a1d6fbe6c
franciscosiq <franciscosiq@gmail.com>
2025-06-26 22:00:42 -0300

    implementando urls

--- pequenium_api/urls.py
+++ pequenium_api/urls.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.permissions import AllowAny
+from pequenium_api.views import UserView, LogoutView
+from newsarticle.views import RecommendedNewsArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-autenticacao/', obtain_auth_token),
     path('categorias/', include('category.urls', namespace='category')),
+    path('deslogar/', LogoutView.as_view(), name='logout'),
+    path('informacoes-de-usuario/', UserView.as_view(), name='user-info'),
+    path('tags/', include('tag.urls', namespace='tag')),
+    path('artigos/', include('newsarticle.urls', namespace='newsarticle')),
+    path('artigos-recomendados/', RecommendedNewsArticleViewSet.as_view({'get': 'list'}, permission_classes=[AllowAny]), name='artigos-recomendados'),
+    path('categorias/', include('category.urls', namespace='category')),
+    path('listas-leitura/', include('readinglist.urls', namespace='readinglist')),
+    path('itens-lista-leitura/', include('readinglistitem.urls', namespace='readinglistitem')),
+    path('interacoes-usuario-artigo/', include('userarticleinteraction.urls', namespace='userarticleinteraction')),
+    path('artigos-tags/', include('newsarticletag.urls', namespace='newsarticletag')),
+    path('artigos-categorias/', include('newsarticlecategory.urls', namespace='newsarticlecategory')),
 ]
+
+urlpatterns += router.urls
+
