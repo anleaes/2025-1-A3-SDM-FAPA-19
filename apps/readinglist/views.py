c5fed8b3ed4ca97350865632ecb50a9a0b39743e
franciscosiq <franciscosiq@gmail.com>
2025-06-27 04:38:29 -0300

    corrigindo reading list views

--- apps/readinglist/views.py
+++ apps/readinglist/views.py
@@ -1,7 +1,16 @@
+@@ -0,0 +1,15 @@
 from .models import ReadingList
 from rest_framework import viewsets
 from .serializer import ReadingListSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ReadingListViewSet(viewsets.ModelViewSet):
-    queryset = ReadingList.objects.all()
+    def get_queryset(self):
+        return ReadingList.objects.filter(user=self.request.user)
     serializer_class = ReadingListSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
