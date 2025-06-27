f7c297e291777848e9d4bdd2ef242a0373edf8c1
franciscosiq <franciscosiq@gmail.com>
2025-06-26 21:51:53 -0300

    criando user article interaction models

---
+++ apps/userarticleinteraction/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UserarticleinteractionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'userarticleinteraction'
+    verbose_name = 'Interações de Usuário com Artigos'
