609f57e20c815163bf54e7091c63e3e050eb34fe
franciscosiq <franciscosiq@gmail.com>
2025-06-27 04:35:51 -0300

    corrigindo news article views

--- apps/newsarticle/views.py
+++ apps/newsarticle/views.py
@@ -1,30 +1,50 @@
 from .models import NewsArticle
 from rest_framework import viewsets, generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from .serializer import NewsArticleSerializer, BasicNewsArticleSerializer
-from django.db.models import Count, Exists, OuterRef, Q, Value, BooleanField, Case, When, Subquery, CharField, IntegerField
+from django.db.models import Count, Exists, OuterRef, Q, Value, BooleanField, Case, When, Subquery, CharField, IntegerField, Prefetch
+from userarticleinteraction.models import UserArticleInteraction
+from readinglistitem.models import ReadingListItem
+from newsarticletag.models import NewsArticleTag
+from newsarticlecategory.models import NewsArticleCategory
+from django.contrib.auth.models import User
 
-class NewsArticleViewSet(viewsets.ModelViewSet):
-    queryset = NewsArticle.objects.all()
-    serializer_class = BasicNewsArticleSerializer
-
-class RecommendedNewsArticleViewSet(viewsets.ModelViewSet):
+class PublicNewsArticleViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = NewsArticleSerializer
-    authentication_classes = []
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny]
 
     def get_queryset(self):
         queryset = NewsArticle.objects.all().defer('content')
-
         queryset = queryset.annotate(
             total_likes=Count('userarticleinteraction', filter=Q(userarticleinteraction__interaction_type='like')),
             total_dislikes=Count('userarticleinteraction', filter=Q(userarticleinteraction__interaction_type='dislike')),
         )
-
-        if self.request.user.is_authenticated:
-            user_interaction_exists = UserArticleInteraction.objects.filter(
-                news_article=OuterRef('pk'),
-                user=self.request.user
-            )
-
         return queryset
+
+class UserNewsArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = NewsArticleSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = NewsArticle.objects.filter(user=self.request.user).defer('content')
+        queryset = queryset.annotate(
+            total_likes=Count('userarticleinteraction', filter=Q(userarticleinteraction__interaction_type='like')),
+            total_dislikes=Count('userarticleinteraction', filter=Q(userarticleinteraction__interaction_type='dislike')),
+        )
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_object(self):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            from rest_framework.exceptions import PermissionDenied
+            raise PermissionDenied("You do not have permission to access this article.")
+        return obj
